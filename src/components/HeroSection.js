c780a373efd2fa294541c83b17093bef347eb0dd
tashenoff <kail.dead.kd@gmail.com>
2025-02-06 14:37:48 +0600

    .

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,6 @@
 // src/components/HeroSection.js
 import React, { useContext } from 'react';
+import { motion } from 'framer-motion'; // Импортируем framer-motion
 import ScrollArrow from './ScrollArrow';
 import WhatsAppButton from './WhatsAppButton';
 import { TranslationContext } from '../TranslationContext';
@@ -8,26 +9,49 @@ function HeroSection() {
   const { polyglot } = useContext(TranslationContext);
 
   return (
-    <div style={{
-      backgroundImage: `url('https://rozmet.ru/local/templates/rozmet/images/front-banner.jpg')`,
-      backgroundSize: 'cover', // Чтобы изображение покрывало весь блок
-      backgroundPosition: 'center', // Чтобы изображение было выровнено по центру
-      backgroundRepeat: 'no-repeat', // Чтобы изображение не повторялось
-    }} id="home" className="hero relative h-[600px] bg-base-200">
-
+    <div
+      style={{
+        backgroundImage: `url('https://rozmet.ru/local/templates/rozmet/images/front-banner.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+      id="home"
+      className="hero relative h-[600px] bg-base-200"
+    >
       <div className="hero-overlay bg-black bg-opacity-80 absolute inset-0"></div>
-
 
       <div className="hero-content text-center relative z-10">
         <div className="w-full flex items-center justify-center flex-col">
-        <img className='w-[200px] lg:hidden mb-10' src='logo.svg' />
+          {/* Логотип с анимацией появления */}
+          <motion.img
+            className="w-[200px] lg:hidden mb-10"
+            src="logo.svg"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+          />
 
-          <h1 className="lg:text-5xl text-3xl text-white/70 font-bold">
+          {/* Заголовок с анимацией появления */}
+          <motion.h1
+            className="lg:text-5xl text-3xl text-white/70 font-bold"
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.2 }} // Задержка анимации для заголовка
+          >
             {polyglot.t('hero.title')}
-          </h1>
-          <p className="py-6 text-white/80">
+          </motion.h1>
+
+          {/* Описание с анимацией появления */}
+          <motion.p
+            className="py-6 text-white/80"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1, delay: 0.4 }} // Задержка анимации для описания
+          >
             {polyglot.t('hero.description')}
-          </p>
+          </motion.p>
+
           <WhatsAppButton />
         </div>
       </div>
